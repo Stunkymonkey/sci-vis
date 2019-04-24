3c37a96e0be7753fc882134f2d86f527601b3ba1
Felix Buehler <account@buehler.rocks>
2019-04-24 14:29:08 +0200

    done ex2-3

---
+++ ex2/assignment2-3.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+files = ["Data" + str(i) + ".csv" for i in range(1, 5)]
+
+for file in files:
+    data = np.genfromtxt(file, delimiter='\t', skip_header=1, dtype=float)
+
+    meanx, meany = np.mean(data, axis=0)
+
+    varx, vary = np.var(data, axis=0)
+
+    covXY = 0
+    for xn, yn in data:
+        covXY += (yn - meany) * (xn - meanx)
+    covXY /= len(data)
+
+    corrXY = covXY / (math.sqrt(varx) * math.sqrt(vary))
+
+    beta2 = covXY / varx
+    beta1 = meany - beta2 * meanx
+
+    xvalues = data[:, 0]
+    startEnd = np.array([min(xvalues), max(xvalues)])
+
+    plt.clf()
+    plt.scatter(data[:, 0], data[:, 1], label="data", color="red")
+
+    plt.plot(startEnd, beta2 * startEnd + beta1, label="linear Regression")
+    plt.plot(startEnd, np.array([meany, meany]), label="Mean", linestyle='--')
+    plt.plot(startEnd, np.array([vary, vary]), label="Variancen", linestyle='-.')
+    plt.plot(startEnd, np.array([corrXY, corrXY]), label="Correlation", linestyle=':')
+
+    plt.ylabel('y')
+    plt.xlabel('x')
+    plt.title(file.replace(".csv", ""))
+    plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+               ncol=2, mode="expand", borderaxespad=0.)
+
+    plt.subplots_adjust(top=0.8)
+    plt.savefig(file.replace("csv", "png"))
+    #plt.show()
